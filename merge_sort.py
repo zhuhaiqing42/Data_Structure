129d6a560b592bd88dfeda80d9acf556ae08fdd9
zhuhaiqing <xqfmy@sina.cn>
2018-04-19 11:08:46 +0800

    modified sum sentence

--- merge_sort.py
+++ merge_sort.py
@@ -1,30 +1,41 @@
+# -*- coding: utf-8 -*-
+
 def merge(a, b):
-    c = []
-    h = j = 0
-    while j < len(a) and h < len(b):
-        if a[j] < b[h]:
-            c.append(a[j])
+    len_a = len(a)
+    len_b = len(b)
+    i, j = 0, 0
+    result = []
+    while i < len_a and j < len_b:
+        if a[i] < b[j]:
+            result.append(a[i])
+            i += 1
+        else:
+            result.append(b[j])
+            j += 1
+    while i < len_a and j < len_b:
+        if b[j] < a[i]:
+            result.append(b[j])
             j += 1
         else:
-            c.append(b[h])
-            h += 1
-
-    if j == len(a):
-        for i in b[h:]:
-            c.append(i)
+            result.append(a[i])
+            i += 1
+    if i == len_a:
+        for k in b[j:]:
+            result.append(k)
     else:
-        for i in a[j:]:
-            c.append(i)
+        for k in a[i:]:
+            result.append(k)
+    return result
 
-    return c
 
-
-def merge_sort(lists):
-    if len(lists) <= 1:
-        return lists
-    middle = len(lists)/2
-    left = merge_sort(lists[:middle])
-    right = merge_sort(lists[middle:])
+def merge_sort(alist):
+    len_alist = len(alist)
+    if len_alist <= 1:
+        return alist
+    else:
+        m = int(len_alist/2)
+        left = merge_sort(alist[:m])
+        right = merge_sort(alist[m:])
     return merge(left, right)
 
 
